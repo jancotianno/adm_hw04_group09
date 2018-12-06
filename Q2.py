9ea8b9d4ca595f1e54fa1ada4187fd6442929313
jancotianno <44699953+jancotianno@users.noreply.github.com>
2018-12-06 19:49:04 +0100

    Add files via upload

--- Q2.py
+++ Q2.py
@@ -27,25 +27,31 @@ def toAscii(s):
 
 diz_ascii = {}
 n_lines = 110000000
-duplicates = 10000000
+duplicates = 1000
 slots = n_lines / duplicates
 #f = pd.read_csv("C:\\Users\\Betglad\\Desktop\\adm_hw04\\passwords2.txt", delimiter="\n")
 
 path = 'C:\\Users\\Betglad\\Desktop\\adm_hw04\\passwords2.txt'
 with open(path, 'r', encoding='utf-8') as f:
     #head = [next(f) for x in range(50)]
-    for s in range(150):
+    for s in range(10000):
         lst = []
         asc = toAscii(next(f))
         lst.append(asc)
-        lst.append(int(asc % slots))        
+        # division method
+        #lst.append(int(asc % slots))       
+        # multiply method
+        w = 32                      # machine word size
+        A = 0.2345                  # 0 <= A <= 1 (or better A to be <= s (with 0 <= s <= 2^w))
+        frac = (asc * A) % 1        # taking fractional part from multiplication
+        frac = int(frac * slots)    # taking the floor part from the multiplication
+        lst.append(frac)
         diz_ascii[next(f)] = lst
         
 diz_slots = {}
 
 for i in range(int(slots)):
-    diz_slots[i] = [k for k, v in diz_ascii.items() if v[1] == i]
-    
+    diz_slots[i] = [k.replace('\n', '') for k, v in diz_ascii.items() if v[1] == i]    
 
 for k, v in diz_slots.items():
     print(k, v)
@@ -55,48 +61,3 @@ for k, v in diz_slots.items():
 #[print(l) for l in head]
 #print(num_lines)    
 #print(len(head))
-
-uguali = []
-diverse = [] 
-n = 1 
-
-s1 = "6<qr=lgh&$uyG5V>C:YT"
-print(toAscii(s1))
-"""print(toAscii(s1) % slots)
-s2 = "!bc;3,rl5m1+N:BRT%px"
-print(toAscii(s2) % slots)
-s3 = "l2*$Sr4T<3QaGfF(hK1Z"
-print(toAscii(s3) % slots)
-s4 = "@gfjLKq5!C8dbxHFEw>+"
-print(toAscii(s4) % slots)
-#lst = [s1, s2, s3, s4]
-
-print(same(s2, s3))"""
-
-"""for st1 in lst:
-    for st2 in lst[1:]:
-        if same(st1, st2):
-            uguali.append(st1)
-            uguali.append(st2)
-        else: diverse.append(st2)"""
-    
-"""for s1 in head:
-    for s2 in head[n:]:
-        if same(s1, s2):
-            uguali.append(s1)
-            #uguali.append(s2)
-            #n+=1
-        else:
-            diverse.append(s1)
-            #diverse.append(s2)
-            #n+=1"""
-
-#print(set(diverse))
-    
-#print(num_lines)
-#for s in diverse:
-#    print(s)
-
-#print(diverse)
-
-    
